5d8d4d098718a108168b6e60af80a5603f4ded67
SimoneScalco <simone.scalco93@gmail.com>
2018-01-18 15:40:23 +0100

    Added toGexf function and case-insensitive edges

--- src/main/scala/tavonatti/scalco/wikipedia_parsing/Main.scala
+++ src/main/scala/tavonatti/scalco/wikipedia_parsing/Main.scala
@@ -2,9 +2,9 @@ package tavonatti.scalco.wikipedia_parsing
 
 import java.util
 
-import org.apache.spark.graphx.{Edge, VertexId}
+import org.apache.spark.graphx.{Edge, Graph, VertexId}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, functions}
 import org.apache.spark.storage.StorageLevel
 
 import scala.util.matching.Regex
@@ -68,7 +68,7 @@ object Main extends App {
     val edgeList=new util.ArrayList[Edge[String]]()
     while (it.hasNext){
       val title=it.next();
-      val temp=df.filter($"title"===title).select("id").collectAsList()
+      val temp=df.filter(functions.lower(df.col("title")).equalTo(title)).select("id").collectAsList() //($"title"===title).select("id").collectAsList()
       if(temp.size()>0) {
         val idEdge: Long =temp.get(0).get(0).asInstanceOf[Long]
         println("Id: " + idEdge + " title: " + title)
@@ -79,6 +79,28 @@ object Main extends App {
     (edgeList.asScala.iterator)
   })
 
-  edges.coalesce(1).saveAsTextFile("outputs/edges4")
+  /* Function used for creating the graph
+   * https://stackoverflow.com/questions/38735413/graphx-visualization
+  */
+  def toGexf[VD,ED](g:Graph[VD,ED]) : String = {
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "<gexf xmlns=\"http://www.gexf.net/1.2draft\" version=\"1.2\">\n" +
+      "  <graph mode=\"static\" defaultedgetype=\"directed\">\n" +
+      "    <nodes>\n" +
+      g.vertices.map(v => "      <node id=\"" + v._1 + "\" label=\"" +
+        v._2 + "\" />\n").collect.mkString +
+      "    </nodes>\n" +
+      "    <edges>\n" +
+      g.edges.map(e => "      <edge source=\"" + e.srcId +
+        "\" target=\"" + e.dstId + "\" label=\"" + e.attr +
+        "\" />\n").collect.mkString +
+      "    </edges>\n" +
+      "  </graph>\n" +
+      "</gexf>"
+  }
+
+  val pageGraph = Graph(nodes, edges)
+
+  println(toGexf(pageGraph))
 
 }
